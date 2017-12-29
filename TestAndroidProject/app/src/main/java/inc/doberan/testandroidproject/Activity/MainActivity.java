1a44905c73a558b1809d511fa97a4bae7b9d0605
鈴木　雅也 <masaya_suzuki@yaz.co.jp>
2017-12-29 11:54:24 +0900

    first commit

---
+++ TestAndroidProject/app/src/main/java/inc/doberan/testandroidproject/Activity/MainActivity.java
@@ -0,0 +1,15 @@
+package inc.doberan.testandroidproject.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import inc.doberan.testandroidproject.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
